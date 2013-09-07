da6aaaa293a3d105530202a95be8f84a4c1b2929
Bruno Coelho <bcs.cin@gmail.com>
2013-09-07 17:29:50 -0300

    Quick union with path compression.

---
+++ Fundamentals/path_compression.js
@@ -0,0 +1,56 @@
+var PathCompression = (function(arrLength) {
+
+	var arr = [],
+      sizes = [],
+	    i = 0;
+
+	for(; i < arrLength; i += 1) {
+		arr[i] = i;
+    sizes[i] = 1;
+	}
+
+	var union = function union(p, q) {
+		var rootP = root(p),
+		    rootQ = root(q);
+
+		if(rootP !== rootQ) {
+			if(sizes[rootP] <= sizes[rootQ]) {
+        arr[rootP] = rootQ;
+        sizes[rootQ] += sizes[rootP];
+      }
+      else {
+        arr[rootQ] = rootP;
+        sizes[rootP] += sizes[rootQ];
+      }
+		}
+	};
+
+	var root = function root(p) {
+		while(p !== arr[p]) {
+      arr[p] = arr[arr[p]];
+			p = arr[p];
+		}
+		return p;
+	};
+
+	var connected = function connected(p, q) {
+		return root(p) === root(q);
+	};
+
+	var print = function print() {
+		console.log("Arr: " + arr);
+	};
+
+	return {
+		union: union,
+		root: root,
+		connected: connected,
+		print: print
+	};
+}(10));
+
+PathCompression.union(1, 2); // [0, 2, 2, 3, 4, 5, 6, 7, 8, 9]
+PathCompression.union(2, 3); // [0, 2, 2, 2, 4, 5, 6, 7, 8, 9]
+PathCompression.union(8, 9); // [0, 2, 2, 2, 4, 5, 6, 7, 9, 9]
+PathCompression.union(5, 2); // [0, 2, 2, 2, 4, 2, 6, 7, 9, 9]
+
