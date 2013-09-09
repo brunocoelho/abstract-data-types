1f2f994b1e3a3b1fad58f14cbb3c7bbae7d9ed31
Bruno Coelho <bcs.cin@gmail.com>
2013-09-09 16:10:52 -0300

    Changing quick union examples.

--- Fundamentals/quick_union.js
+++ Fundamentals/quick_union.js
@@ -36,7 +36,10 @@ QuickUnion.prototype = {
     }
 };
 
-QuickUnion.union(1,2); // [0, 2, 2, 3, 4, 5, 6, 7, 8, 9]
-QuickUnion.union(2,3); // [0, 2, 3, 3, 4, 5, 6, 7, 8, 9]
-QuickUnion.union(5,2); // [0, 2, 2, 3, 4, 3, 6, 7, 8, 9]
+var q = new QuickUnion(10);
+
+q.union(1, 2); // [0, 2, 2, 3, 4, 5, 6, 7, 8, 9]
+q.union(2, 3); // [0, 2, 3, 3, 4, 5, 6, 7, 8, 9]
+q.union(5, 2); // [0, 2, 2, 3, 4, 3, 6, 7, 8, 9]
+q.connected(5, 2); // true
 
