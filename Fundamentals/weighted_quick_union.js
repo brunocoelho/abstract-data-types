aa597539c90b03d591019fd973003fde70a1775f
Bruno Coelho <bcs.cin@gmail.com>
2013-09-09 14:56:21 -0300

    Changing weighted quick union.

--- Fundamentals/weighted_quick_union.js
+++ Fundamentals/weighted_quick_union.js
@@ -1,56 +1,57 @@
-var WeightedQuickUnion = (function(arrLength) {
+var WeightedQuickUnion = function WeightedQuickUnion(arrLength) {
 
-	var arr = [],
-      sizes = [],
-	    i = 0;
+    this.arrLength = arrLength;
+    this.arr = [];
+    this.sizes = [];
 
-	for(; i < arrLength; i += 1) {
-		arr[i] = i;
-    sizes[i] = 1;
-	}
+    var i = 0;
 
-	var union = function union(p, q) {
-		var rootP = root(p),
-		    rootQ = root(q);
+    for(; i < arrLength; i += 1) {
+        this.arr[i] = i;
+        this.sizes[i] = 1;
+    }
+};
 
-		if(rootP !== rootQ) {
-			if(sizes[rootP] <= sizes[rootQ]) {
-        arr[rootP] = rootQ;
-        sizes[rootQ] += sizes[rootP];
-      }
-      else {
-        arr[rootQ] = rootP;
-        sizes[rootP] += sizes[rootQ];
-      }
-		}
-	};
+WeightedQuickUnion.prototype = {
 
-	var root = function root(p) {
-		while(p !== arr[p]) {
-			p = arr[p];
-		}
-		return p;
-	};
+    union: function union(p, q) {
+        var rootP = this.root(p),
+            rootQ = this.root(q);
 
-	var connected = function connected(p, q) {
-		return root(p) === root(q);
-	};
+        if(rootP !== rootQ) {
+            if(this.sizes[rootP] >= this.sizes[rootQ]) {
+                this.arr[rootQ] = rootP;
+                this.sizes[rootP] += this.sizes[rootQ];
+            }
+            else {
+                this.arr[rootP] = rootQ;
+                this.sizes[rootQ] += this.sizes[rootP];
+            }
+        }
+    },
 
-	var print = function print() {
-		console.log("Arr: " + arr);
-		console.log("Sizes: " + sizes);
-	};
+    root: function root(p) {
+        while(p !== this.arr[p]) {
+            p = this.arr[p];
+        }
+        return p;
+    },
 
-	return {
-		union: union,
-		root: root,
-		connected: connected,
-		print: print
-	};
-}(10));
+    connected: function connected(p, q) {
+        return this.root(p) === this.root(q);
+    },
 
-WeightedQuickUnion.union(1,2); // [0, 2, 2, 3, 4, 5, 6, 7, 8, 9]
-WeightedQuickUnion.union(2,3); // [0, 2, 2, 2, 4, 5, 6, 7, 8, 9]
-WeightedQuickUnion.union(8,9); // [0, 2, 2, 2, 4, 5, 6, 7, 9, 9]
-WeightedQuickUnion.union(5,2); // [0, 2, 2, 2, 4, 2, 6, 7, 9, 9]
+    print: function print() {
+        console.log('Arr: ' + this.arr);
+        console.log('Sizes: ' + this.sizes);
+    }
+};
+
+var w = new WeightedQuickUnion(10);
+
+w.union(1, 2); // [0, 2, 2, 3, 4, 5, 6, 7, 8, 9]
+w.union(2, 3); // [0, 2, 2, 2, 4, 5, 6, 7, 8, 9]
+w.union(8, 9); // [0, 2, 2, 2, 4, 5, 6, 7, 9, 9]
+w.union(5, 2); // [0, 2, 2, 2, 4, 2, 6, 7, 9, 9]
+w.connected(8, 9); // true
 
