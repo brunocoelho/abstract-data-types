7e33b5feebcb1c8875bc1ac919d7754019b6e699
Bruno Coelho <bcs.cin@gmail.com>
2013-09-24 00:32:04 -0300

    Insertion sort.

---
+++ Sorting/insertion_sort.js
@@ -0,0 +1,32 @@
+function InsertionSort (arr) {
+	var i,
+		j,
+		len = arr.length,
+		swap;
+
+	for (i = 0; i < len; i++) {
+		for (j = i; j > 0; j--) {
+			if (arr[j] < arr[j - 1]) {
+				swap = arr[j];
+				arr[j] = arr[j - 1];
+				arr[j - 1] = swap;
+			}
+			else {
+				break;
+			}
+		}
+	}
+	return arr;
+}
+
+/*
+
+new InsertionSort([5, 4, 3, 2, 1]);
+# [1, 2, 3, 4, 5]
+
+For randomly ordered arrays of length N with with distinct keys,
+insertion sort uses ~N2/4 compares and ~N2/4 exchanges on the average.
+The worst case is ~ N2/2 compares and ~ N2/2 exchanges and
+the best case is N-1 compares and 0 exchanges.
+
+*/
