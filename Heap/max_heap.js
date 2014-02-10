88cff6a5f6d87a2111510e0455dd266aa40ff2b6
Bruno Coelho <bcs.cin@gmail.com>
2014-02-09 23:15:52 -0300

    Change Heap name to MaxHeap.

--- Heap/max_heap.js
+++ Heap/max_heap.js
@@ -1,13 +1,13 @@
-function Heap() {
+function MaxHeap() {
 	this.heap = [undefined];
 }
 
-Heap.prototype.add = function(data) {
+MaxHeap.prototype.add = function(data) {
 	var lastPositionIndex = this.heap.push(data) - 1;
 	this._bubbleUp(lastPositionIndex);
 };
 
-Heap.prototype.delete = function() {
+MaxHeap.prototype.delete = function() {
 	var lastPositionIndex = this.heap.length - 1;
 
 	this.heap[1] = this.heap[lastPositionIndex];
@@ -16,17 +16,17 @@ Heap.prototype.delete = function() {
 	this._bubbleDown();
 };
 
-Heap.prototype.top = function() {
+MaxHeap.prototype.top = function() {
 	return this.heap[1];
 };
 
-Heap.prototype._swap = function(lastPositionIndex, parentIndex) {
+MaxHeap.prototype._swap = function(lastPositionIndex, parentIndex) {
 	var temp = this.heap[lastPositionIndex];
 	this.heap[lastPositionIndex] = this.heap[parentIndex];
 	this.heap[parentIndex] = temp;
 };
 
-Heap.prototype._bubbleUp = function(lastPositionIndex) {
+MaxHeap.prototype._bubbleUp = function(lastPositionIndex) {
 	var lastData = this.heap[lastPositionIndex],
 		parentIndex = Math.floor(lastPositionIndex / 2),
 		temp;
@@ -38,7 +38,7 @@ Heap.prototype._bubbleUp = function(lastPositionIndex) {
 	}
 };
 
-Heap.prototype._bubbleDown = function() {
+MaxHeap.prototype._bubbleDown = function() {
 	var index = 1,
 		firstData = this.heap[index];
 
@@ -47,11 +47,11 @@ Heap.prototype._bubbleDown = function() {
 	}
 };
 
-Heap.prototype._hasChild = function(index) {
+MaxHeap.prototype._hasChild = function(index) {
 	return this.heap[index * 2] || this.heap[(index * 2) + 1];
 };
 
-Heap.prototype._lesserThanAnyChild = function(index) {
+MaxHeap.prototype._lesserThanAnyChild = function(index) {
 	var firstChild = this.heap[index * 2],
 		parent = this.heap[index];
 		secondChild = this.heap[(index * 2) + 1];
@@ -59,7 +59,7 @@ Heap.prototype._lesserThanAnyChild = function(index) {
 	return parent < firstChild || parent < secondChild;
 };
 
-Heap.prototype._swapBiggerChild = function(index) {
+MaxHeap.prototype._swapBiggerChild = function(index) {
 	var firstChildIndex = index * 2,
 		firstChild = this.heap[firstChildIndex],
 		secondChildIndex = (index * 2) + 1,
@@ -70,8 +70,8 @@ Heap.prototype._swapBiggerChild = function(index) {
 			this._swap(secondChildIndex, index);
 };
 
-Heap.prototype.print = function() {
+MaxHeap.prototype.print = function() {
 	console.log(this.heap.slice(1));
 };
 
-exports.Heap = Heap;
+exports.MaxHeap = MaxHeap;
